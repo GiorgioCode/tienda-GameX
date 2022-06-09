74ba085f6c9f87b5e03d4b9e9e9f7df691534aee
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-06-09 20:25:00 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './GameX-Logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-2xl font-bold text-black">
+      Proximamente, aquí encontrarás una gran variedad de juegos y recursos para Gamers. 
+    </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
