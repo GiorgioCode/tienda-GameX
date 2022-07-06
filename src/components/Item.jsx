ba229b91f412b43102fca6e9a6e31b4942c7a3a8
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-07-06 18:41:09 -0300

    modif cards 2

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,7 @@ const Item = ({ games }) => {
   return (
     <div className="m-3 mx-1 bg-slate-900 rounded-xl shadow-md overflow-hidden max-w-2xl">
       <div className="flex">
-        <div className="">
+        <div className="shrink-0">
           <img
             className="object-cover h-full w-48"
             src={games.imagen}
@@ -13,17 +13,17 @@ const Item = ({ games }) => {
           />
         </div>
         <div className="p-8">
-          <div className=" text-white text-2xl text-center mb-3 w-44">
+          <div className=" text-white text-2xl text-center mb-3 w-44 text-violet-600">
             {games.nombre}
           </div>
           <div className=" text-white mb-3"> Disponibles: {games.stock}</div>
-          <div className=" text-white mb-3 text-2xl">
+          <div className=" text-white mb-3 text-2xl bg-black text-center py-2 text-violet-600 rounded-md">
             {" "}
-            Precio: $ {games.precio}
+            $ {games.precio}
           </div>
 
           <Link to={`/detalle/${games.id}`}>
-            <button className="btn px-16 my-6 text-center btn-outline rounded-full circle hover:bg-primary bg-slate-900 text-white hover:text-white hover:shadow-lg hover:shadow-primary">
+            <button className="btn px-16 my-1 w-48 text-center btn-outline rounded-full circle hover:bg-primary bg-slate-900 text-white hover:text-white hover:shadow-lg hover:shadow-primary">
               {" "}
               Ver mas
             </button>
