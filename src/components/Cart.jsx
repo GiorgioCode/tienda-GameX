065470808ec7569d5ae8c4daf94cb2ad6fd78dbe
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-07-09 10:04:01 -0300

    modificacion cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../context/CartContext";
 import ItemCart from "./ItemCart";
+import carritoVacio from "./carritoVacio.svg";
 
 const Cart = () => {
     const { carrito, totalPrice } = useCartContext();
@@ -11,11 +12,18 @@ const Cart = () => {
         return (
             <>
                 <h2 className="mt-32 text-center text-3xl">
-                    ¡El carrito está vacio!
+                    <div className="d-flex inline-flex flex-wrap w-full justify-center">
+                        <img
+                            className=""
+                            src={carritoVacio}
+                            alt="carrito vacio"
+                        />
+                    </div>
+                    El carrito está vacio y triste...
                 </h2>
-                <div className="d-flex inline-flex flex-wrap w-full justify-center mt-12 mb-80">
+                <div className="d-flex inline-flex flex-wrap w-full justify-center mt-2 mb-80">
                     <Link to={`/`}>
-                        <button className="btn px-16 my-1 w-48 text-center btn-outline rounded-full circle hover:bg-primary bg-slate-900 text-white hover:text-white hover:shadow-lg hover:shadow-primary">
+                        <button className="btn px-16 text-center btn-outline rounded-full circle hover:bg-primary bg-slate-900 text-white hover:text-white hover:shadow-lg hover:shadow-primary">
                             {" "}
                             Ver Catalogo
                         </button>
