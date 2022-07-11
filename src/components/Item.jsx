a1b4eebcb4566f106589cfc8443d841f49b724ba
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-07-11 20:49:38 -0300

    modificacion tarjetas

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,37 +3,32 @@ import { Link } from "react-router-dom";
 
 const Item = ({ games }) => {
     return (
-        <div className="m-2 hover:shadow-red-600 w-96 h-1/3 mx-1 bg-slate-900 rounded-xl shadow-xl overflow-hidden max-w-2xl">
-            <div className="flex">
-                <div className="shrink-0">
-                    <img
-                        className="object-cover h-full w-48"
-                        src={games.imagen}
-                        alt="portada"
-                    />
-                </div>
-                <div className="p-8">
-                    <div className="text-2xl text-center mb-1 w-full  text-violet-600">
-                        {games.nombre}
+        <Link className="h-1/3" to={`/detalle/${games.id}`}>
+            <div className="m-2 hover:shadow-red-600 w-96  mx-1 hover:bg-red-900 bg-slate-900 rounded-xl shadow-xl overflow-hidden max-w-2xl">
+                <div className="flex">
+                    <div className="shrink-0">
+                        <img
+                            className="object-cover h-full w-48"
+                            src={games.imagen}
+                            alt="portada"
+                        />
                     </div>
-                    <div className=" text-white mb-3">
-                        {" "}
-                        Disponibles: {games.stock}
-                    </div>
-                    <div className="text-4xl text-center py-1 text-orange-600">
-                        {" "}
-                        $ {games.precio}
-                    </div>
-
-                    <Link to={`/detalle/${games.id}`}>
-                        <button className="btn my-0.5 w-full text-center btn-outline rounded-full circle hover:bg-primary bg-slate-900 text-white hover:text-white hover:shadow-lg hover:shadow-primary">
+                    <div className="p-8">
+                        <div className="text-2xl text-center mb-1 w-full  text-violet-600">
+                            {games.nombre}
+                        </div>
+                        <div className=" text-white mb-3">
                             {" "}
-                            ver mas
-                        </button>
-                    </Link>
+                            Disponibles: {games.stock}
+                        </div>
+                        <div className="text-4xl text-center py-1 text-orange-600">
+                            {" "}
+                            $ {games.precio}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
